d113bb041f95347d47ffae2f5d785bab8e9ff1c4
lackhurt <pangjn@yeah.net>
2016-06-04 11:28:00 +0800

    删除没有的import

--- src/main/java/TestRpc.java
+++ src/main/java/TestRpc.java
@@ -1,18 +1,5 @@
-import com.evernote.edam.type.Notebook;
-import com.note.migration.*;
 import com.note.migration.Consumer;
-import com.note.migration.evernote.EvernotePuller;
 import com.note.migration.evernote.FetchNotebooksProcessor;
-import com.oracle.javafx.jmx.json.impl.JSONMessages;
-import com.rabbitmq.client.*;
-import com.rabbitmq.tools.json.JSONReader;
-import com.rabbitmq.tools.json.JSONWriter;
-import com.sun.deploy.util.StringUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by lackhurt on 16/5/29.
